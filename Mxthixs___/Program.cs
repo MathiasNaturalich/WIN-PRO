299d90f85f2c74ef3ac4997ea2de00f952c9ff68
MathiasNaturalich <N00479733@upn.pe>
2025-12-03 18:17:05 -0500

    Add files via upload

---
+++ Mxthixs___/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mxthixs___
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Asigne un numero aleatorio");
+        }
+    }
+}
